104d87c6b2378740d326142164c626eed26e3c82
Paul St.Germain <paulg.stgermain@gmail.com>
2024-08-01 12:00:00 -0400

    Update knex configuration for Heroku

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 
-const pgConnection = process.env.DATABASE_URL
+const pgConnection = process.env.DATABASE_URL;
 
 module.exports = {
   development: {
+    client: 'pg',
+    connection: pgConnection,
+    migrations: {
+      directory: './src/db/migrations',
+    },
+    seeds: {
+      directory: './src/db/seeds',
+    },
+  },
+  production: {
     client: 'pg',
     connection: pgConnection,
     migrations: {

a5ee59f7c49d8cfb4f4b0b0ae27e92bfe5ffb313
Paul St.Germain <paulg.stgermain@gmail.com>
2024-08-15 22:22:18 -0400

    adds cors, ensures correct auth flow from FE to BE

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jobRoutes = require('./routes/jobRoutes');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', jobRoutes);
 
